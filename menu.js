87c329d6bcdd7af198950aaa5cd2c3beb797ebb6
ren-wen <zhangrenwen0107@gmail.com>
2023-01-04 23:18:33 +0800

    feat(css/index.html): add normalize/ dynamic menu

---
+++ menu.js
@@ -0,0 +1,119 @@
+const menu = [
+  {
+    id: "2",
+    title: "chapter2 ES6 基礎元素",
+    submenu: [
+      {
+        id: "2-1",
+        title: "2-1 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+      {
+        id: "2-2",
+        title: "2-2 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "chapter3 ES6 基礎元素",
+    submenu: [
+      {
+        id: "3-1",
+        title: "3-1 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+      {
+        id: "3-2",
+        title: "3-2 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "chapter3 ES6 基礎元素",
+    submenu: [
+      {
+        id: "3-1",
+        title: "3-1 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+      {
+        id: "3-2",
+        title: "3-2 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "chapter3 ES6 基礎元素",
+    submenu: [
+      {
+        id: "3-1",
+        title: "3-1 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+      {
+        id: "3-2",
+        title: "3-2 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "chapter3 ES6 基礎元素",
+    submenu: [
+      {
+        id: "3-1",
+        title: "3-1 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+      {
+        id: "3-2",
+        title: "3-2 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "chapter3 ES6 基礎元素",
+    submenu: [
+      {
+        id: "3-1",
+        title: "3-1 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+      {
+        id: "3-2",
+        title: "3-2 物件實字",
+        href: "./chapter2/2-1.html",
+      },
+    ],
+  },
+];
+
+function createMenu(submenu, el) {
+  submenu.map((o) => {
+    if (o.href) {
+      const a = document.createElement("a");
+      const li = document.createElement("li");
+      a.href = o.href;
+      a.textContent = o.title;
+      li.appendChild(a);
+      el.appendChild(li);
+    }
+    if (o.submenu) {
+      const ul = document.createElement("ul");
+      ul.textContent = o.title;
+      el.appendChild(ul);
+      createMenu(o.submenu, ul);
+    }
+  });
+}
+
+createMenu(menu, document.querySelector("aside"));
