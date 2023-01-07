2b00e34c994f55c1b808e74a1a8ce96feb0033f3
ren-wen <zhangrenwen0107@gmail.com>
2023-01-07 17:38:12 +0800

    feat(menu.js): new menu content

--- menu.js
+++ menu.js
@@ -18,85 +18,107 @@ const menu = [
         title: "2-3 解構賦值",
         href: "./2-3.html",
       },
-    ],
-  },
-  {
-    id: "3",
-    title: "chapter3 ES6 基礎元素",
-    submenu: [
       {
-        id: "3-1",
-        title: "3-1 物件實字",
-        href: "./2-1.html",
+        id: "2-4",
+        title: "2-4 其餘參數和展開運算子",
+        href: "#",
       },
       {
-        id: "3-2",
-        title: "3-2 物件實字",
-        href: "./2-1.html",
-      },
-    ],
-  },
-  {
-    id: "3",
-    title: "chapter3 ES6 基礎元素",
-    submenu: [
-      {
-        id: "3-1",
-        title: "3-1 物件實字",
-        href: "./2-1.html",
+        id: "2-5",
+        title: "2-5 字串樣板",
+        href: "#",
       },
       {
-        id: "3-2",
-        title: "3-2 物件實字",
-        href: "./2-1.html",
+        id: "2-6",
+        title: "2-6 let 和 const 敘述",
+        href: "#",
       },
     ],
   },
   {
     id: "3",
-    title: "chapter3 ES6 基礎元素",
+    title: "chapter3 類別、符號、物件和修飾器",
     submenu: [
       {
         id: "3-1",
-        title: "3-1 物件實字",
-        href: "./2-1.html",
+        title: "3-1 類別",
+        href: "#",
       },
       {
         id: "3-2",
-        title: "3-2 物件實字",
-        href: "./2-1.html",
+        title: "3-2 符號",
+        href: "#",
       },
     ],
   },
   {
-    id: "3",
-    title: "chapter3 ES6 基礎元素",
+    id: "4",
+    title: "chapter4",
     submenu: [
       {
-        id: "3-1",
-        title: "3-1 物件實字",
-        href: "./2-1.html",
+        id: "4-1",
+        title: "4-1",
+        href: "#",
       },
       {
-        id: "3-2",
-        title: "3-2 物件實字",
-        href: "./2-1.html",
+        id: "4-2",
+        title: "4-2",
+        href: "#",
       },
     ],
   },
   {
-    id: "3",
-    title: "chapter3 ES6 基礎元素",
+    id: "5",
+    title: "chapter5",
     submenu: [
       {
-        id: "3-1",
-        title: "3-1 物件實字",
-        href: "./2-1.html",
+        id: "5-1",
+        title: "5-1",
+        href: "#",
       },
+    ],
+  },
+  {
+    id: "6",
+    title: "chapter6",
+    submenu: [
       {
-        id: "3-2",
-        title: "3-2 物件實字",
-        href: "./2-1.html",
+        id: "6-1",
+        title: "6-1",
+        href: "#",
+      },
+    ],
+  },
+  {
+    id: "7",
+    title: "chapter7",
+    submenu: [
+      {
+        id: "7-1",
+        title: "7-1",
+        href: "#",
+      },
+    ],
+  },
+  {
+    id: "8",
+    title: "chapter8",
+    submenu: [
+      {
+        id: "8-1",
+        title: "8-1",
+        href: "#",
+      },
+    ],
+  },
+  {
+    id: "9",
+    title: "chapter9",
+    submenu: [
+      {
+        id: "9-1",
+        title: "9-1",
+        href: "#",
       },
     ],
   },
