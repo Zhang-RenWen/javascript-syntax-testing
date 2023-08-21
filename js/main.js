efcc647dfb61c30f5748ca59afbe6a42ccb6884e
ren-wen <zhangrenwen0107@gmail.com>
2023-08-21 16:54:14 +0800

    feat(16-1.html): add 16-1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-import {inserted} from './clipboard.js'
-import  {initMenu} from'./menu.js'
-import  {initCodeLine} from './codeline.js'
+import { initMenu } from'./menu.js'
+import { initClipboard } from './clipboard.js'
+import { initCodeLine } from './codeline.js'
 initMenu()
-inserted(document)
 hljs.highlightAll();
+initClipboard(document)
 initCodeLine()
